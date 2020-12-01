478d1fa2c3a041fe7579107e5b30d13250f4d851
Artem <example@gmail.com>
2020-12-01 07:23:30 +0300

    script on films

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
 "use strict";
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+const personalMiovieDB = {
+    count: numberOfFilms,
+    movies :{},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените кго?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('На сколько оцените его?', '');
+
+personalMiovieDB.movies[a] = b;
+personalMiovieDB.movies[c] = d;
+
+console.log(personalMiovieDB);
